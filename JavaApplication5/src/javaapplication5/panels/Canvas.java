c5868bde0265873e938f1c913a0d8de64f3533e5
AlbFR <albertoferrada20@gmail.com>
2022-12-01 00:59:24 -0300

    MouseListener correctly tested, now we know how it works

--- JavaApplication5/src/javaapplication5/panels/Canvas.java
+++ JavaApplication5/src/javaapplication5/panels/Canvas.java
@@ -10,46 +10,44 @@ import javax.swing.border.Border;
 public class Canvas extends JPanel implements MouseListener {
     private ArrayList<Point> puntos;
 
-   public Canvas() {
-      Border border = BorderFactory.createLineBorder(Color.green, 3);
-      this.setBounds(0, 0, 700, 500);
-      this.setBackground(Color.black);
-      this.setBorder(border);
-      puntos = new ArrayList<Point>();
-   }
-   @Override
-   public void paintComponent(Graphics g) {
-      super.paintComponent(g);
-   }
+    public Canvas() {
+        Border border = BorderFactory.createLineBorder(Color.green, 3);
+        this.setBounds(0, 0, 700, 500);
+        this.setBackground(Color.white);
+        this.setBorder(border);
+        this.addMouseListener(this);
+        puntos = new ArrayList<Point>();
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.black);
+        for (int i=0;i<puntos.size();++i) {
+            g.drawRect(puntos.get(i).x, puntos.get(i).y, 3, 3);
+        }
+    }
 
     @Override
     public void mouseClicked(java.awt.event.MouseEvent me) {
-        System.out.println("Hola xd");
-        System.out.println(me);
-        // this.repaint();
+        puntos.add(new Point(me.getX(), me.getY()));
+        this.repaint();
     }
 
     @Override
     public void mousePressed(java.awt.event.MouseEvent me) {
-        System.out.println("Hola xd");
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
     public void mouseReleased(java.awt.event.MouseEvent me) {
-        System.out.println("Hola xd");
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
     public void mouseEntered(java.awt.event.MouseEvent me) {
-        System.out.println("Hola xd");
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
     }
 
     @Override
     public void mouseExited(java.awt.event.MouseEvent me) {
-        System.out.println("Hola xd");
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
     }
 }
